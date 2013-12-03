6565cbacbcfbb558abbd69d98cd72af6f79b0613
awsam <xyu.wang.0713@gmail.com>
2013-12-03 23:12:11 +0800

    btcc and bter is available

--- bter.rb
+++ bter.rb
@@ -12,6 +12,7 @@ require 'json'
 require 'rest-open-uri'
 require 'digest/sha2'
 require 'digest/hmac'
+require 'rest_client'
 require 'debugger'
 
 class Bter
@@ -32,34 +33,50 @@ class Bter
   end
 
   def money_balance
-#    params = URI.encode_www_form 'a'=> 1
-#    sign = get_sign params
     b = balance['available_funds']
     money = (b.size != 0) ? b['CNY'] : nil
     money ? money.to_f : 0.0
   end
 
   def self.btc_price
-    site = open @btc_price_address
+    res = RestClient.get @btc_price_address
 
-    if !site || site.status[0] != '200'
-      raise 'Bter连接网站失败'
+    if res.code != 200
+      error = '连接Bter网站失败'
+      Log.error error
+      raise error
     end
 
-    result = JSON.parse site.string
+    result = JSON.parse res.to_str
 
     if result['result'] != 'true' || !result['avg'] || !result['sell'] || !result['buy'] || result['avg'] <= 0 || result['sell'] <= 0 || result['buy'] <= 0
-      raise '无法从Bter获取价格信息'
+      error = '无法从Bter获取价格信息'
+      Log.error error
+      raise error
     end
 
     return {"avg" => result['avg'], "sell"=> result['sell'], 'buy' => result['buy']}
-  rescue Exception => e
-    puts e and Log.error e
   end
 
   def buy price, amount
     res = post @place_order_address, URI.encode_www_form(pair: "btc_cny", type: "BUY", rate: price, amount: amount)
-    puts res.string
+    if !res || res['result'] != true
+      error = "在Bter买比特币失败 原因：#{res["msg"]}"
+      Log.error error
+      raise error
+    end
+    res
+  end
+
+  def sell price, amount
+    res = post @place_order_address, URI.encode_www_form(pair: "btc_cny", type: "SELL", rate: price, amount: amount)
+
+    if !res || res['result'] != true
+      error = "在Bter买比特币失败 原因：#{res['msg']}"
+      Log.error error
+      raise error
+    end
+    res
   end
 
   private
@@ -68,32 +85,33 @@ class Bter
   end
 
   def post url, data
-    puts data
-    open url, 'KEY' => @key, :method => :post, 'SIGN' => get_sign(data) { data}
-  end
-
-  def balance
-    site = post @balance_address, ''
-
-    if !site || site.status[0] != '200'
+    result = RestClient.post url, data,  'KEY' => @key, 'SIGN' => get_sign(data)
+    if result.code != 200
       raise '连接Bter网站失败'
     end
 
-    result = JSON.parse site.string
+    JSON.parse result.to_str
+  end
 
-    if result['result'] != 'true' 
+  def balance
+    result = post @balance_address, ''
+
+    if result['result'] != 'true'
       raise '无法从Bter获取价格信息  ' + result['message']
     end
 
     return result
 
   rescue Exception => e
-    puts e and Log.error e
+    puts e
+    Log.error e
   end
 end
-
+=begin
 b = Bter.new '86EB2B7B-848A-423E-8E63-5FAC295193AB', '08b78689ef43f6f23deb6d2125d841e7c3cb799652137cd45501c095c2d2bbb1'
 puts b.money_balance
 puts b.btc_balance
-debugger
 puts Bter.btc_price
+debugger
+a = 10
+=end
